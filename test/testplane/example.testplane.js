597b297e1dec6aa517ff2cf3b9672cb29661fc8c
BulBat <bulatkhayrullintermin@mail.ru>
2024-07-05 13:07:26 +0300

    first commit

--- test/testplane/example.testplane.js
+++ test/testplane/example.testplane.js
@@ -2,6 +2,6 @@ describe('github', function() {
     it('should check repository name', async ({ browser }) => {
         await browser.url('https://github.com/gemini-testing/testplane');
 
-        await expect(browser.$('#readme h1')).toHaveText('Testplane (ex-Hermione)');
+        await expect(browser.$(".Link--primary")).toHaveText(".github");
     });
 });
---
+++ test/testplane/serverList.testplane.js
@@ -0,0 +1,34 @@
+const BASE_URL = "http://localhost:3000/hw/store";
+const BUG_ID = 2;
+
+const url = Boolean(BUG_ID)
+  ? `${BASE_URL}/catalog?bug_id=${BUG_ID}`
+  : `${BASE_URL}/catalog`;
+
+describe("Каталог", () => {
+  it(`в каталоге должны отображаться товары, список которых приходит с сервера + отображают название, цена и ссылка на страницу с подробной информацией о товаре`, async ({
+    browser,
+  }) => {
+    await browser.url(url);
+
+    const products = await browser.$$(".ProductItem");
+
+    await expect(products.length).toBeGreaterThan(
+      0,
+      "Товары в каталоге не найдены"
+    );
+
+    for (let product of products) {
+      const productName = await product.$(".ProductItem-Name");
+      const productPrice = await product.$(".ProductItem-Price");
+      const productLink = await product.$(".ProductItem-DetailsLink");
+
+      await expect(productName).toBeDisplayed();
+      await expect(productPrice).toBeDisplayed();
+      await expect(productLink).toBeDisplayed();
+
+      const linkHref = await productLink.getAttribute("href");
+      await expect(linkHref).toMatch(/\/hw\/store\/catalog\/\d+/);
+    }
+  });
+});
